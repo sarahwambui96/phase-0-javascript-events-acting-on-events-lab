47dcdc0ada17abcb155d8e7f34539d338c5a0100
sarahwambui96 <sarahwambui843@gmail.com>
2023-03-11 11:14:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+//moveDodgerLeft function
+function moveDodgerLeft(){
+    const leftNumbers = dodger.style.left.replace ("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (left > 0){
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+// write our event listener
+document.addEventListener("keydown", function(event){
+    if (event.key === "ArrowLeft"){
+        moveDodgerLeft();
+    }
+});
+
+//moveDodgerRight function
+function moveDodgerRight(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (left > 0){
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+//write out event listener
+document.addEventListener("keydown", function(event) {
+    if(event.key === "ArrowRight");
+    moveDodgerRight();
+});
